95ff4dd5ecd651368a2fe97495330e45d387dccb
Vinicius da Conceição Teixeira <viniciuscteixeira.rj@gmail.com>
2023-08-23 18:45:29 -0300

    fix

--- resources/constants.ts
+++ resources/constants.ts
@@ -36,7 +36,17 @@ true;
 `;
 
 export const WebviewTooKit = `
-document.addEventListener("DOMContentLoaded", function(e) {
+function ready(callback){
+    // in case the document is already rendered
+    if (document.readyState!='loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function(){
+        if (document.readyState=='complete') callback();
+    });
+}
+ready(function() {
     let hidden = true;
     
     const toolButton = document.createElement('a');
